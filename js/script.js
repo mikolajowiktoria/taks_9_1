3592a24105c38d43128daffdb68fd3b21fa05e63
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-01-03 18:35:25 +0100

    add consol.log

--- js/script.js
+++ js/script.js
@@ -6,13 +6,16 @@ function getTriangleArea(a,h) {
     if (a > 0 && h > 0) {
          return a*h/2;
     }
-else {
-  console.log("Nieprawidłowe dane");
-}
+    else {
+        return("Nieprawidłowe dane");
+    }
    
 }
 
 console.log(getTriangleArea(10, 6));
+console.log(triangle2Area);
+console.log(triangle3Area);
+alert(triangle1Area);
 
 
 
