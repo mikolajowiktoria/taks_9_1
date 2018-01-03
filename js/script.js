49eff4aa9ce43b579ba7b4d7c0ed8f76a1a848e9
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-01-03 17:49:41 +0100

    Delete console.log in js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,13 @@ var triangle3Area = getTriangleArea(40, 0);
 
 function getTriangleArea(a,h) {
     if (a > 0 && h > 0) {
-       console.log(a*h/2);
+         return a*h/2;
     }
 else {
   console.log("Nieprawidłowe dane");
 }
-    return a*h/2;
+   
 }
-
 
 console.log(getTriangleArea(10, 6));
 
