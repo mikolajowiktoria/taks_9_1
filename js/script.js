e9b0862c54cbf3b490d7b4f25e10110dfcc0f596
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-01-03 13:51:38 +0100

    add js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var triangle1Area = getTriangleArea(10, 21);
+var triangle2Area = getTriangleArea(39, 1);
+var triangle3Area = getTriangleArea(40, 0);
+
+function getTriangleArea(a,h) {
+    if (a > 0 && h > 0) {
+       console.log(a*h/2);
+    }
+else {
+  console.log("Nieprawidłowe dane");
+}
+    return a*h/2;
+}
+function getTriangleArea(a,h) {
+    if (a > 0 && h > 0) {
+       console.log(a*h/2);
+    }
+else {
+  console.log("Nieprawidłowe dane");
+}
+    return a*h/2;
+}
+function getTriangleArea(a,h) {
+    if (a > 0 && h > 0) {
+       console.log(a*h/2);
+    }
+else {
+  console.log("Nieprawidłowe dane");
+}
+    return a*h/2;
+}
+
+console.log(getTriangleArea(10, 6));
+
+
+
